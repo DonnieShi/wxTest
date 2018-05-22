24c46492d14b9b61669fd5f00242c6c86f73edee
wchshi <1004778046@qq.com>
2018-05-22 18:30:00 +0800

    添加滚动view

--- pages/home/home.js
+++ pages/home/home.js
@@ -12,19 +12,41 @@ Component({
    */
   data: {
     imgUrls:[
-      "img/one_swiper/1.jpg",
-      "img/one_swiper/2.jpg",
-      "img/one_swiper/3.jpg",
-      "img/one_swiper/4.jpg",
-      "img/one_swiper/5.jpg",
-      "img/one_swiper/6.jpg"
-    ]
+      "../../img/one_swiper/1.jpg",
+      "../../img/one_swiper/2.jpg",
+      "../../img/one_swiper/3.jpg",
+      "../../img/one_swiper/4.jpg",
+      "../../img/one_swiper/5.jpg",
+      "../../img/one_swiper/6.jpg"
+    ],
+    boolHidden:true,
+    detailSrc:""
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    imgLoad:function(e){
 
+    },
+    swiperClick:function(e){
+      console.log(e)
+      var that=this;
+      that.setData({
+        boolHidden:false,
+        detailSrc:(that.data.imgUrls[e.target.id])
+      })
+
+    },
+    onLoad:function(){
+
+    },
+    detailClick:function(e){
+      var that=this;
+      that.setData({
+        boolHidden:true
+      })
+    }
   }
 })
